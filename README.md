5f8afc8d5fbdb9cab0797c53638428ab973849f7
plkpiotr <plk.piotr.pp@gmail.com>
2019-07-09 01:41:31 +0200

    Add syntax of TypeScript and watching all files through Nodemon

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
-### stock-exchange-backend
-
 `npm start` or `ts-node server.ts` to run backend on localhost:8080
 
-`nodemon` to real time running backend on localhost:8080 in dev mode
+`nodemon` to real time running backend on localhost:8080 in development mode
 
-### In the future...
+### To do:
 
 Create handling errors after "sign up" implementing (4/14)
--- app.ts
+++ app.ts
@@ -2,24 +2,29 @@ import * as express from 'express';
 import * as morgan from 'morgan';
 import * as mongoose from 'mongoose';
 
-const app = express();
+class App {
+    public app: express.Application = express();
 
-mongoose.connect('mongodb+srv://stock-exchange:stock-exchange@stock-exchange-btfeh.mongodb.net/test?retryWrites=true&w=majority', {
-    useMongoClient: true
-});
+    public constructor() {
+        App.connectWithDatabase();
+        this.configureApp();
+    }
 
-app.use(morgan('dev'));
+    private configureApp(): void {
+        this.app.use(morgan('dev'));
+        this.app.use((request, response, next): void => {
+            response.header('Access-Control-Allow-Origin', '*');
+            response.header('Access-Control-Allow-Headers', 'Authorization');
+            next();
+        });
+    }
 
-app.use((request, response): void => {
-    response.header('Access-Control-Allow-Origin', '*');
-    response.header('Access-Control-Allow-Headers', 'Authorization');
-    // next();
-});
+    private static connectWithDatabase(): void {
+        mongoose.connect('mongodb+srv://stock-exchange:stock-exchange@stock-exchange-btfeh.mongodb.net/' +
+            'test?retryWrites=true&w=majority', {
+            useNewUrlParser: true
+        });
+    }
+}
 
-app.use((request, response): void => {
-    response.status(200).json({
-        message: 'OK'
-    });
-});
-
-export default app;
+export default new App().app;

03b5ed2407a5e62707b243469c5f7f69a31d7476
plkpiotr <plk.piotr.pp@gmail.com>
2019-07-08 19:11:13 +0200

    Add Nodemon, Morgan and Mongoose

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# stock-exchange-backend
+### stock-exchange-backend
 
-`npm start` to run backend on localhost:8080
+`npm start` or `ts-node server.ts` to run backend on localhost:8080
+
+`nodemon` to real time running backend on localhost:8080 in dev mode
+
+### In the future...
+
+Create handling errors after "sign up" implementing (4/14)
--- app.ts
+++ app.ts
@@ -1,5 +1,20 @@
-import * as express from 'express'
+import * as express from 'express';
+import * as morgan from 'morgan';
+import * as mongoose from 'mongoose';
+
 const app = express();
+
+mongoose.connect('mongodb+srv://stock-exchange:stock-exchange@stock-exchange-btfeh.mongodb.net/test?retryWrites=true&w=majority', {
+    useMongoClient: true
+});
+
+app.use(morgan('dev'));
+
+app.use((request, response): void => {
+    response.header('Access-Control-Allow-Origin', '*');
+    response.header('Access-Control-Allow-Headers', 'Authorization');
+    // next();
+});
 
 app.use((request, response): void => {
     response.status(200).json({
